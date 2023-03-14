a5df3d1325e97c6c7aa8db0f5debdafea1326083
Aleksey Lesnikov <poprobuyu@mail.ru>
2023-03-14 14:36:26 +0500

    Update serializers

--- test_task/network/serializers.py
+++ test_task/network/serializers.py
@@ -10,7 +10,6 @@ class ContactsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contacts
         fields = '__all__'
-        read_only_fields = ('id',)
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -19,7 +18,6 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
-        read_only_fields = ('id',)
 
 
 class DistributorSerializer(serializers.ModelSerializer):
@@ -28,17 +26,19 @@ class DistributorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Distributor
         fields = '__all__'
-        read_only_fields = ('id',)
 
 
 class LinkSerializer(serializers.ModelSerializer):
     """Сериализатор модели Link"""
+
+    id = serializers.IntegerField()
 
     distributor = serializers.SlugRelatedField(
         slug_field='title',
         queryset=Distributor.objects.all()
     )
     supplier = serializers.SlugRelatedField(
+        required=False,
         slug_field='title',
         queryset=Distributor.objects.all()
     )
@@ -51,13 +51,57 @@ class LinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Link
         fields = '__all__'
-        read_only_fields = ('id', 'debt', 'created',)
+        read_only_fields = ('debt', 'created',)
 
 
 class NetworkSerializer(serializers.ModelSerializer):
     """Сериализатор модели Network"""
 
+    manufacturer = LinkSerializer(validators=[ManufacturerValidator()])
+    distributor_1 = LinkSerializer()
+    distributor_2 = LinkSerializer()
+
     class Meta:
         model = Network
         fields = '__all__'
-        read_only_fields = ('id',)
+
+    def create(self, validated_data: dict) -> Network:
+        #: Данные звеньев, которые необходимо добавить в сеть
+        manufacturer_data = validated_data.pop('manufacturer')
+        distributor_1_data = validated_data.pop('distributor_1')
+        distributor_2_data = validated_data.pop('distributor_2')
+
+        #: Получение звеньев для включения в сеть
+        manufacturer = Link.objects.get(
+            id=manufacturer_data.pop('id'),
+            distributor=manufacturer_data.pop('distributor'),
+        )
+        distributor_1 = Link.objects.get(id=distributor_1_data.pop('id'),)
+        distributor_2 = Link.objects.get(id=distributor_2_data.pop('id'),)
+
+        #: Создание сети
+        instance = Network.objects.create(
+            manufacturer=manufacturer,
+            distributor_1=distributor_1,
+            distributor_2=distributor_2,
+            **validated_data
+        )
+        return instance
+
+    def update(self, instance: Network, validated_data: dict) -> Network:
+        #: Данные для обновления полей
+        manufacturer_data = validated_data.pop('manufacturer')
+        distributor_1_data = validated_data.pop('distributor_1')
+        distributor_2_data = validated_data.pop('distributor_2')
+
+        #: Обновление значений полей
+        instance.manufacturer = Link.objects.get(
+            id=manufacturer_data.pop('id', instance.manufacturer.id),
+            distributor=manufacturer_data.pop('distributor', instance.manufacturer.distributor)
+        )
+        instance.distributor_1 = Link.objects.get(id=distributor_1_data.pop('id', instance.distributor_1.id),)
+        instance.distributor_2 = Link.objects.get(id=distributor_2_data.pop('id', instance.distributor_2.id),)
+        instance.title = validated_data.get('title', instance.title)
+        instance.save()
+
+        return instance
